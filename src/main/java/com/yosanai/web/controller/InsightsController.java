06e8a2ff343e9416c9b7fdbc0cf104a24a58e4c2
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2017-01-03 18:43:01 -0800

    rest controller

--- src/main/java/com/yosanai/web/controller/InsightsController.java
+++ src/main/java/com/yosanai/web/controller/InsightsController.java
@@ -1,17 +1,17 @@
 package com.yosanai.web.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mastercard.api.core.model.RequestMap;
 import com.mastercard.api.sectorinsights.Insights;
 import com.mastercard.api.sectorinsights.Parameters;
 import com.yosanai.web.model.InsightsRequest;
 
-@Controller
+@RestController
 public class InsightsController {
 
 	@RequestMapping(value = "/insights", method = RequestMethod.POST, produces = "application/json")
